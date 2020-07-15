dae938837cefdf8cb17b4d901471d196440d5a67
Paras Sharma <git@eparas.com>
2020-07-14 21:06:21 -0500

    prod api url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://sigmoidpayapiserver.azurewebsites.net/",
+  apiUrl: "https://paylens-api-server.azurewebsites.net/",
   plaidApiUrl: "https://development.plaid.com",
   plaidPublicKey: "066eab548902d1532836b65fa0c7be"
 };
